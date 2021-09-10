97fb5b15c789bbce518735f0cbb27e89b858ccd2
1798365869@qq.com <1798365869@qq.com>
2021-09-10 09:40:42 +0800

    upload IOSOrMacOS v1.3.1

---
+++ IOSOrMacOS/IOSMacBrainLinkProSDKSpecialEditonV1.3.1/Mac_HZLBlue4.0Demo/MAC_HZLBlue4.0Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MAC_HZLBlue4.0Demo
+//
+//  Created by 方亮 on 2018/6/24.
+//  Copyright © 2018年 macrotellect. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ IOSOrMacOS/IOSMacBrainLinkProSDKSpecialEditonV1.3.1/Mac_HZLBlue4.0Demo/MAC_HZLBlue4.0Demo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MAC_HZLBlue4.0Demo
+//
+//  Created by 方亮 on 2018/6/24.
+//  Copyright © 2018年 macrotellect. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
